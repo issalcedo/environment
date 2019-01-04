d9e677874e9ad355369295ddbd3fe11545893c9a
Isidro Salcedo <isidro.x.salcedo.-nd@disney.com>
2019-01-04 14:30:35 -0800

    inital setup

---
+++ setup-osx.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# install brew
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install iterm2
+
+brew cask install iterm2
+
+# Change default shell to zsh
+
+chsh -s /bin/zsh
+
+# Install oh my zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install zsh auto completion
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# mv default rc 
+mv ~/.zshrc ~/.zshrc-old
+
+# create a symlink for our rc
+ln zshrc /Users/salci003/.zshrc
